da8de8e6d07259389a72c95bfc7b276c63bfe55f
Pardhu-Guttula <drpardhu007@gmail.com>
2024-08-03 00:52:15 +0530

    made changes in deploy.sh scripting check

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,35 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Stop the most recently created container if it is running
-CONTAINER_ID=$(docker ps -lq)
-echo "Most recently created container ID: $CONTAINER_ID"
-if [ -n "$CONTAINER_ID" ]; then
-    echo "Stopping the running container..."
-    docker stop ${CONTAINER_ID}
-    echo "Container stopped."
-fi
+# # Stop the most recently created container if it is running
+# CONTAINER_ID=$(docker ps -lq)
+# echo "Most recently created container ID: $CONTAINER_ID"
+# if [ -n "$CONTAINER_ID" ]; then
+#     echo "Stopping the running container..."
+#     docker stop ${CONTAINER_ID}
+#     echo "Container stopped."
+# fi
 
-# Define the Docker image names
-FE_IMAGE1="pardhuguttula/userbot"
-FE_IMAGE2="pardhuguttula/agentchat-fe"
-BE_IMAGE="pardhuguttula/agentchat-be"
+# # Define the Docker image names
+# FE_IMAGE1="pardhuguttula/userbot"
+# FE_IMAGE2="pardhuguttula/agentchat-fe"
+# BE_IMAGE="pardhuguttula/agentchat-be"
 
-# Get the latest Docker tags from DockerHub
-FE_TAG1=$(curl -s "https://hub.docker.com/v2/repositories/${FE_IMAGE1}/tags/" | jq -r '.results[0].name')
-FE_TAG2=$(curl -s "https://hub.docker.com/v2/repositories/${FE_IMAGE2}/tags/" | jq -r '.results[0].name')
-BE_TAG=$(curl -s "https://hub.docker.com/v2/repositories/${BE_IMAGE}/tags/" | jq -r '.results[0].name')
+# # Get the latest Docker tags from DockerHub
+# FE_TAG1=$(curl -s "https://hub.docker.com/v2/repositories/${FE_IMAGE1}/tags/" | jq -r '.results[0].name')
+# FE_TAG2=$(curl -s "https://hub.docker.com/v2/repositories/${FE_IMAGE2}/tags/" | jq -r '.results[0].name')
+# BE_TAG=$(curl -s "https://hub.docker.com/v2/repositories/${BE_IMAGE}/tags/" | jq -r '.results[0].name')
 
-# Pull the images with the dynamically determined tags
-echo "Pulling image ${FE_IMAGE1}:${FE_TAG1}..."
-docker pull "${FE_IMAGE1}:${FE_TAG1}"
+# # Pull the images with the dynamically determined tags
+# echo "Pulling image ${FE_IMAGE1}:${FE_TAG1}..."
+# docker pull "${FE_IMAGE1}:${FE_TAG1}"
 
-echo "Pulling image ${FE_IMAGE2}:${FE_TAG2}..."
-docker pull "${FE_IMAGE2}:${FE_TAG2}"
+# echo "Pulling image ${FE_IMAGE2}:${FE_TAG2}..."
+# docker pull "${FE_IMAGE2}:${FE_TAG2}"
 
-echo "Pulling image ${BE_IMAGE}:${BE_TAG}..."
-docker pull "${BE_IMAGE}:${BE_TAG}"
-
+# echo "Pulling image ${BE_IMAGE}:${BE_TAG}..."
+# docker pull "${BE_IMAGE}:${BE_TAG}"
+echo "Checking for docker-compose.yml..."
+ls -l ./docker-compose.yml
 # Update docker-compose.yml with the latest tags
 echo "Updating docker-compose.yml with the latest tags..."
 sed -i "s|image: ${FE_IMAGE1}:latest|image: ${FE_IMAGE1}:${FE_TAG1}|g" ./docker-compose.yml
